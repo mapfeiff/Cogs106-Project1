8204e1eb2955c1a2b81eff66cab13cb0b76c2fea
mapfeiff <mapfeiff@uci.edu>
2023-01-30 19:33:57 -0800

    No comment specified; using default commit. Updated using file update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo "hello world!"
+#pull from github repository to ensure the local directory is up to date
+git pull
 
-echo "$1"
+#save the time to the version file
+date > version
+
+#add all the new/modified files to stage them for the commit
+git add --all
+
+#commit the changes made on the local drive
+git commit -m "${1:-No comment specified; using default commit. Updated using file update-version.sh}"
+
+#push the changes to github
+git push
